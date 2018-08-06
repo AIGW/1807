32a484727a5f6c6022a62c75572185160beedc0f
414296787 <414296787@qq.com>
2018-08-06 20:16:18 +0800

    1

--- 02.01day/09-批量重命名.py
+++ 02.01day/09-批量重命名.py
@@ -1,7 +1,8 @@
 import os
 dir = input("请输入批量重命名的文件夹名字")
-files = os.listdir(dir)
+files = os.listdir(dir) #列表
 os.chdir(dir)
+
 for i in files:
 	position = i.rfind(".")
 	newname = i[:position]+"-腾讯"+i[position:]
